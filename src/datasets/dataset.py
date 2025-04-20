90b596f1aab0c37d9560d03066d1e8d95c8a1300
paulbuttler <pbuttler@student.ethz.ch>
2025-04-20 18:45:21 +0200

    changed dataset class

---
+++ src/datasets/dataset.py
@@ -0,0 +1,114 @@
+import os
+import torch
+import gzip
+import pickle
+from pathlib import Path
+import kornia.augmentation as K
+from torchvision.io import decode_image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.utils import make_grid
+from datasets.transforms import denormalize, random_roi_transform, apply_roi_transform, AppearanceAugmentation
+from utils.rottrans import aa2rot, matrix_to_rotation_6d
+
+class SynDataset(Dataset):
+
+    def __init__(self, 
+                 img_dir:str, 
+                 body_meta_dir:str, 
+                 mode="train", 
+                 device="cuda"):
+        self.img_dir = img_dir
+        with gzip.open(body_meta_dir, "rb") as f:
+            self.body_meta = pickle.load(f)
+        self.uids = list(self.body_meta.keys())
+        self.train = True if mode=="train" else False
+        self.device = device
+        self.appearance_aug = AppearanceAugmentation().to(device) if self.train else None
+        self.normalize = K.Normalize(
+            mean=torch.tensor([0.485, 0.456, 0.406]),
+            std=torch.tensor([0.229, 0.224, 0.225])
+        ).to(device)
+
+    def __len__(self):
+        return len(self.body_meta)
+
+    def __getitem__(self, idx):
+        uid = self.uids[idx]
+        img_path = os.path.join(self.img_dir, f"img_{uid}.jpg")
+
+        img = (decode_image(img_path).float().to(self.device) / 255.0).unsqueeze(0)
+
+        kp2d = torch.tensor(
+            self.body_meta[uid]["ldmks_2d"], dtype=torch.float32, device=self.device
+        ).unsqueeze(0)
+
+        roi = torch.tensor(
+            self.body_meta[uid]["roi"], dtype=torch.float32, device=self.device
+        ).unsqueeze(0)
+
+        if self.train and self.appearance_aug:
+            img, kp2d = random_roi_transform(img, kp2d, roi, "train")
+            img = self.appearance_aug(img)
+        else:
+            img, kp2d = apply_roi_transform(img, kp2d, roi, "test")
+
+        # Normalize with ImageNet stats
+        img = self.normalize(img)
+
+        # Normalize 2D landmark coordinates to [0, 1]
+        kp2d = kp2d/img.shape[-1]
+
+        # In addition, we need to convert axis angle to 6d rotation
+        pose = torch.from_numpy(self.body_meta[uid]["pose"]).to(device=self.device, dtype=torch.float32) # Local axis angle representation!!
+        shape = torch.from_numpy(self.body_meta[uid]["shape"][:10]).to(device=self.device, dtype=torch.float32) # only first 10 elements
+
+        target = {
+            'landmarks': kp2d.squeeze(0),
+            'pose': pose,
+            'shape': shape,
+        }
+
+        return img.squeeze(0), target
+
+# Test the dataset
+if __name__ == "__main__":
+    dataset = SynDataset(
+        img_dir=Path("data/raw/synth_body"),
+        body_meta_dir="data/annotations/body_meta.pkl.gz",
+        mode="train",
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    )
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+
+    print(f"Number of samples: {len(dataset)}")
+    print(f"Number of batches: {len(dataloader)}")
+
+    images, labels = next(iter(dataloader))
+    print(f"Image shape: {images.shape}")
+
+    import cv2
+
+    images, labels = next(iter(dataloader))
+    images = denormalize(images)     # [B, C, H, W]
+    kp2d = (labels["landmarks"] * images.shape[-1]).cpu().numpy()
+
+    # Visualize a batch of images
+    grid = make_grid(images, nrow=6, padding=2)  # [C, H', W']
+    grid_np = grid.permute(1, 2, 0).cpu().numpy()  # [H', W', C]
+
+    grid_bgr = cv2.cvtColor(grid_np, cv2.COLOR_RGB2BGR)
+
+    cv2.imshow('Batch Grid', grid_bgr)
+    cv2.waitKey(0)
+
+    # Visualize a single image with 2d landmarks
+    idx = 3
+
+    img = images[idx].permute(1, 2, 0).cpu().numpy()
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # Draw keypoints
+    for (x, y) in kp2d[idx]:
+        x, y = int(x.item()), int(y.item())
+        cv2.circle(img, (x, y), radius=1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
+    cv2.imshow("Image with Landmarks", img)
+    cv2.waitKey(0)
