ea48a4755071f29f0480ef8e3df9869ed293453c
paulbuttler <pbuttler@student.ethz.ch>
2025-05-14 20:36:19 +0200

    update readme

--- job.sh
+++ job.sh
@@ -5,11 +5,11 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gpus=rtx_3090:1
-#SBATCH --time=3-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --output=training_log_%j.out
 
 source ~/.bashrc
 
 workon dslab
 cd src
-python scripts/train.py
+python training/train.py
