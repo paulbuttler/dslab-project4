07768fbb1280f09d65ad8d73f80a576592672581
paulbuttler <pbuttler@student.ethz.ch>
2025-05-14 11:50:44 +0200

    update ldmk gen script

--- src/utils/extract_roi.py
+++ src/utils/extract_roi.py
@@ -1,21 +1,20 @@
 import argparse
-import pickle
-import gzip
+import joblib
 import numpy as np
 import cv2
 from pathlib import Path
 
 
-def compute_roi(landmarks, image=None, size=512):
+def compute_roi(landmarks, image=None, margin=0.08, input_size=512):
     """Crop a square ROI that tightly contains all landmarks."""
     x_min, y_min = np.min(landmarks, axis=0)
     x_max, y_max = np.max(landmarks, axis=0)
 
     center_x = (x_min + x_max) / 2
     center_y = (y_min + y_max) / 2
-    # 8% padding seems to be enough to contain all bodyparts even in weird poses
-    box_size = max(x_max - x_min, y_max - y_min) * 1.08
-    box_size = min(size, box_size)
+
+    box_size = max(x_max - x_min, y_max - y_min) * (1.0 + margin)
+    box_size = min(input_size, box_size)
 
     x_min = int(np.floor(center_x - box_size / 2))
     y_min = int(np.floor(center_y - box_size / 2))
@@ -23,7 +22,7 @@ def compute_roi(landmarks, image=None, size=512):
     y_max = int(np.ceil(center_y + box_size / 2))
 
     if image is not None:
-        # Visualize ROI and landmarks on the image
+        # Visualize ROI and landmarks
         cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
         for x, y in landmarks:
             cv2.circle(image, (int(x), int(y)), 1, (0, 255, 0), -1)
@@ -41,10 +40,10 @@ if __name__ == "__main__":
     parser.add_argument("--fidx", type=int, default=None)
     args = parser.parse_args()
 
-    args.data_dir = Path("data/raw/synth_body")
+    args.data_dir = (Path("data/raw/synth_body") if not args.hand else Path("data/raw/synth_hand"))
 
     if args.sidx is None:
-        args.sidx = np.random.randint(0, 1000)
+        args.sidx = np.random.randint(0, 20000)
     if args.fidx is None:
         args.fidx = np.random.randint(0, 5)
 
@@ -55,13 +54,14 @@ if __name__ == "__main__":
         print(f"File not found: {img_file}")
         exit(1)
 
-    with gzip.open("data/annotations/body_ldmks_roi.pkl.gz", "rb") as f:
-        landmarks_dict = pickle.load(f)
-    landmarks = landmarks_dict[uid]
+    if not args.hand:
+        landmarks_dict = joblib.load("data/annotations/body_ldmks_roi.pkl")
+        landmarks = landmarks_dict[uid]
+        margin = 0.08
+    else:
+        hand_meta = joblib.load("data/annotations/hand_meta.pkl")
+        landmarks = hand_meta[uid]["ldmks_2d"]
+        margin = 0.05
 
-    # with gzip.open("data/annotations/body_meta.pkl.gz", "rb") as f:
-    #     body_dict = pickle.load(f)
-    # landmarks = body_dict[uid]["ldmks_2d"]
     img = cv2.imread(str(img_file))
-
-    _ = compute_roi(landmarks, img)
+    _ = compute_roi(landmarks, img, margin, img.shape[-2])
